e24ad761c692c51b01289f2b5bb3fe4feeb05610
yuta20253 <magee.20253@gmail.com>
2025-04-15 10:51:16 +0900

    add: php基礎課題１・２

---
+++ php-practice_2.php
@@ -0,0 +1,115 @@
+<?php
+// Q1 tic-tac問題
+for ($i = 1; $i < 101; $i++) {
+    if ($i % 4 == 0 && $i % 5 == 0) {
+        echo "tic-tac" . PHP_EOL;
+    } elseif ($i % 4 == 0) {
+        echo "tic" . PHP_EOL;  
+    } elseif ($i % 5 == 0) {
+        echo "tac" . PHP_EOL; 
+    } else {
+       echo $i . PHP_EOL;
+    }
+}
+
+// Q2 多次元連想配列
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel' => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel' => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel' => '09055556666'
+    ],
+];
+
+//1.
+echo "{$personalInfos[1]['name']}の電話番号は{$personalInfos[1]['tel']}です。" . PHP_EOL;
+
+//2.
+foreach ($personalInfos as $key => $value) {
+    $num = $key + 1;
+    echo "{$num}番目の{$value['name']}のメールアドレスは{$value['mail']}で、電話番号は{$value['tel']}です。" . PHP_EOL;
+}
+
+//3.
+$ageList = [25, 30, 18];
+
+foreach ($ageList as $key => $value) {
+    echo $value . PHP_EOL;
+    $personalInfos[$key]['age'] = $value;
+}
+
+var_dump($personalInfos);
+
+
+// Q3 オブジェクト-1
+class Student 
+{
+    public $studentId;
+    public $studentName;
+    
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+    
+    public function attend()
+    {
+        echo '授業に出席しました。';    
+    }
+}
+
+$student = new Student(1, '山本');
+echo "学籍番号{$student->studentId}番の生徒は{$student->studentName}です。";
+
+
+// Q4 オブジェクト-2
+class Student 
+{
+    public $studentId;
+    public $studentName;
+    
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+    
+    public function attend($subject)
+    {
+        echo "{$this->studentName}は{$subject}の授業に参加しました。学籍番号：{$this->studentId}";    
+    }
+}
+
+$student = new Student(1, '山本');
+$student->attend('PHP');
+
+
+// Q5 定義済みクラス
+//1.
+$dt = new DateTime();
+print $dt->format('Y-m-d') . PHP_EOL;
+
+$dt->modify('- 1 month');
+print $dt->format('Y-m-d') . PHP_EOL;
+
+//2.
+$dt = new DateTime();
+print $dt->format('Y-m-d') . PHP_EOL;
+
+$beforeDate = new DateTime('1992-4-25');
+print $beforeDate->format('Y-m-d') . PHP_EOL;
+
+$interval = $dt->diff($beforeDate, true)->days;
+echo "あの日から{$interval}日経過しました。" . PHP_EOL;
+
---
+++ user-class-php.php
@@ -0,0 +1,31 @@
+<?php
+
+class User 
+{
+    protected $name;
+    public $email;
+
+    public function __construct($paramName, $paramEmail)
+    {
+        $this->name = $paramName;
+        $this->email = $paramEmail;
+    }
+
+    public function sayHi()
+    {
+        echo 'Hi! my name is ' . $this->name;
+    }
+}
+
+class JapaneseUser extends User 
+{
+    public function JapaneseGreet()
+    {
+        echo 'こんにちは';
+    }
+
+    public function sayHi()
+    {
+        echo 'やあ！俺は' . $this->name . '!開発王になる男だ！';
+    }
+}
